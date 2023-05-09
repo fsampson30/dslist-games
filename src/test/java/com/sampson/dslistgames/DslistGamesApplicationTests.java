3ebd34d9c3a9680afd398c174bb7d87117df85f3
fsampson30 <fsampson30@gmail.com>
2023-05-08 21:21:05 -0300

    Initial commit

---
+++ src/test/java/com/sampson/dslistgames/DslistGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sampson.dslistgames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
