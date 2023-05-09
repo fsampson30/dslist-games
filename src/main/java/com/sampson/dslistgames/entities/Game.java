64053bf7c6222bff78a61f7a6d71b0721b44ade1
fsampson30 <fsampson30@gmail.com>
2023-05-08 21:52:34 -0300

    Game seed

--- src/main/java/com/sampson/dslistgames/entities/Game.java
+++ src/main/java/com/sampson/dslistgames/entities/Game.java
@@ -16,19 +16,24 @@ public class Game {
     private Integer year;
     private String genre;
     private String platforms;
+
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataforms, String imgUrl, String shortDescription, String longDescription) {
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl, String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platforms = plataforms;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -74,6 +79,14 @@ public class Game {
         this.platforms = platforms;
     }
 
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
     public String getImgUrl() {
         return imgUrl;
     }
@@ -97,6 +110,8 @@ public class Game {
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
+
+
 
     @Override
     public boolean equals(Object o) {
