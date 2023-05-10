1e434c17ba7c0b5f32a75181d8423f7b15c62d13
fsampson30 <fsampson30@gmail.com>
2023-05-09 21:29:14 -0300

    GET game by ID

--- src/main/java/com/sampson/dslistgames/controllers/GameController.java
+++ src/main/java/com/sampson/dslistgames/controllers/GameController.java
@@ -1,9 +1,11 @@
 package com.sampson.dslistgames.controllers;
 
+import com.sampson.dslistgames.dto.GameDTO;
 import com.sampson.dslistgames.dto.GameMinDTO;
 import com.sampson.dslistgames.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,5 +21,10 @@ public class GameController {
     @GetMapping
     public List<GameMinDTO> findAll(){
         return gameService.findAll();
+    }
+
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id){
+        return gameService.findById(id);
     }
 }
--- src/main/java/com/sampson/dslistgames/services/GameService.java
+++ src/main/java/com/sampson/dslistgames/services/GameService.java
@@ -1,9 +1,11 @@
 package com.sampson.dslistgames.services;
 
+import com.sampson.dslistgames.dto.GameDTO;
 import com.sampson.dslistgames.dto.GameMinDTO;
 import com.sampson.dslistgames.repositories.GameRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,8 +15,14 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         return gameRepository.findAll().stream().map(x -> new GameMinDTO(x)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long gameId) {
+        return new GameDTO(gameRepository.findById(gameId).get());
     }
 
 
